dfe1ffc0c3055c16bb96f108c5de1e949563cfd7
Andres Loja <cal-107@hotmail.com>
2018-07-06 12:33:11 -0500

    Cambios-sidebar

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -3,7 +3,7 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">TABLAS</li>
-            <li>
+            <li class="{{ Request::is('mantenimiento/ambitos*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/ambitos')}}">
                     <i class="fa fa-umbrella"></i> <span>Ambitos</span>
                 </a>
@@ -18,13 +18,13 @@
                     <i class="fa fa-building"></i> <span>Empresas</span>
                 </a>
             </li>--}}
-            <li>
+            <li class="{{ Request::is('mantenimiento/objetivos*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/objetivos')}}">
                     <i class="fa fa-check-square-o"></i> <span>Objetivos</span>
                 </a>
             </li>
             @if(Auth::user()->role == 1)
-            <li>
+            <li class="{{ Request::is('mantenimiento/plantillas*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/plantillas')}}">
                     <i class="fa fa-file-word-o"></i> <span>Plantillas</span>
                 </a>
@@ -35,7 +35,7 @@
                     <i class="fa fa-indent"></i> <span>Parrafos</span>
                 </a>
             </li>--}}
-            <li>
+            <li class="{{ Request::is('mantenimiento/politicas*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/politicas')}}">
                     <i class="fa fa-legal"></i> <span>Politicas</span>
                 </a>
@@ -45,18 +45,18 @@
                     <i class="fa fa-suitcase"></i> <span>Representantes</span>
                 </a>
             </li>--}}
-            <li>
+            <li class="{{ Request::is('mantenimiento/riesgos*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/riesgos')}}">
                     <i class="fa fa-warning"></i> <span>Riesgos</span>
                 </a>
             </li>
-            <li>
+            <li class="{{ Request::is('mantenimiento/tiporiesgo*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/tiporiesgo')}}">
                     <i class="fa fa-list-ul"></i> <span>Tipo de Riesgos</span>
                 </a>
             </li>
             @if(Auth::user()->role == 1)
-            <li>
+            <li class="{{ Request::is('mantenimiento/usuarios*') ? 'active' : '' }}">
                 <a href="{{URL::to('/mantenimiento/usuarios')}}">
                     <i class="fa fa-users"></i> <span>Usuarios</span>
                 </a>
